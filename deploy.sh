e0fa1f184ef4258a48529ac9c9cb02d2ce5d85eb
Nicholas Pringle <nicholaschristopherpringle@gmail.com>
2020-02-23 00:10:00 +0100

    Added travis and deploy script

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+docker build -t nicholaschris/multi-client:latest nicholaschris/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t nicholaschris/multi-server:latest nicholaschris/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t nicholaschris/multi-worker:latest nicholaschris/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push nicholaschris/multi-client:latest
+docker push nicholaschris/multi-server:latest
+docker push nicholaschris/multi-worker:latest
+
+docker push nicholaschris/multi-client:$SHA
+docker push nicholaschris/multi-server:$SHA
+docker push nicholaschris/multi-worker:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/client-deployment server=nicholaschris/multi-client:$SHA
+kubectl set image deployments/server-deployment server=nicholaschris/multi-server:$SHA
+kubectl set image deployments/worker-deployment server=nicholaschris/multi-worker:$SHA
