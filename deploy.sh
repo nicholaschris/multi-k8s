29dec83640d0aa4bf6aa1414a800975e9129e752
Nicholas Pringle <nicholaschristopherpringle@gmail.com>
2020-02-23 00:46:19 +0100

    Debug deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,17 @@
-echo $SHA
-docker build -t nicholaschris/multi-client:latest nicholaschris/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t nicholaschris/multi-server:latest nicholaschris/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t nicholaschris/multi-worker:latest nicholaschris/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+# echo $SHA
+# docker build -t nicholaschris/multi-client:latest nicholaschris/multi-client:$SHA -f ./client/Dockerfile ./client
+# docker build -t nicholaschris/multi-server:latest nicholaschris/multi-server:$SHA -f ./server/Dockerfile ./server
+# docker build -t nicholaschris/multi-worker:latest nicholaschris/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
-docker push nicholaschris/multi-client:latest
-docker push nicholaschris/multi-server:latest
-docker push nicholaschris/multi-worker:latest
+# docker push nicholaschris/multi-client:latest
+# docker push nicholaschris/multi-server:latest
+# docker push nicholaschris/multi-worker:latest
 
-docker push nicholaschris/multi-client:$SHA
-docker push nicholaschris/multi-server:$SHA
-docker push nicholaschris/multi-worker:$SHA
+# docker push nicholaschris/multi-client:$SHA
+# docker push nicholaschris/multi-server:$SHA
+# docker push nicholaschris/multi-worker:$SHA
 
-kubectl apply -f k8s
-kubectl set image deployments/client-deployment client=nicholaschris/multi-client:$SHA
-kubectl set image deployments/server-deployment server=nicholaschris/multi-server:$SHA
-kubectl set image deployments/worker-deployment worker=nicholaschris/multi-worker:$SHA
+# kubectl apply -f k8s
+# kubectl set image deployments/client-deployment client=nicholaschris/multi-client:$SHA
+# kubectl set image deployments/server-deployment server=nicholaschris/multi-server:$SHA
+# kubectl set image deployments/worker-deployment worker=nicholaschris/multi-worker:$SHA
